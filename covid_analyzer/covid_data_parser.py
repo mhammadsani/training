5065073e73077cd2f6d085ca5458536304ddfdd9
Muhammad Hammad Sani <hammad.sani@arbisoft.com>
2023-08-23 14:34:51 +0500

    style: fixed some style issues (indentation, EOL etc)

--- covid_analyzer/covid_data_parser.py
+++ covid_analyzer/covid_data_parser.py
@@ -13,25 +13,23 @@ def parse_arguments() -> object:
     parser = argparse.ArgumentParser()
     parser.add_argument(
         COVID_FILES_PATH, help="data_dir"
-        )
+    )
     parser.add_argument(
         RECOVERED_RATIO, '--recovered_ratio', help="To find the recovered ratio of country"
-        )
+    )
     parser.add_argument(
         SAFETY_MEASURE_ARG, '--safety_measure', help="To find average death rate"
-        )
+    )
     parser.add_argument(
         MOST_EFFICIENT_SAFETY_MEASURES, '--most_efficient_safetey_measures',
         action="store_true", help="To find top 5 efficient safety measures"
-        )
+    )
     parser.add_argument(
         GRAPHICAL_DISPLAY, '--graphical_display_of_efficient_safety_measures'
         ,action="store_true", help="See a bar plot of most efficient safety measures"
         )
     args = parser.parse_args()    
     return args
-
-
 
 
 def read_files(covid_data: dict, covid_analyzer_file_path: str) -> None:
